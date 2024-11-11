acc2fe65ac0f7985678cd8e67319a69e7fff023d
Fhurky <furkocal@gmail.com>
2024-11-11 18:58:50 +0300

    Cleaning

--- VGRS.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("games_genres.csv")
-data = data.loc[:, ~data.columns.str.contains('^Unnamed', na=False)]
-data = data.drop(["Genres"], axis = 1)
-
-print(data.columns)
---
+++ new_system.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+
+data = pd.read_csv("games_steam.csv")
+
+scaler = StandardScaler()
+
+data = data.drop([
+    "Grid-Based Movement", "Logic", "Pirates", "Tennis", "Faith", "Time Attack", "Motorbike", 
+    "Hockey", "Tanks", "Nonlinear", "Kickstarter", "Building", "Sailing", "Solitaire"
+    , "Intentionally Awkward Controls", "Psychological", 
+    "Dating Sim", "6DOF", "Top-Down Shooter", "Web Publishing", "RTS", "Space Sim", 
+    "Capitalism", "Music-Based Procedural Generation", "Hack and Slash", "Skating", 
+    "Memes", "Dynamic Narration", "Text-Based", "Clicker", "Noir", "Cold War", 
+    "Martial Arts", "Lovecraftian", "Mystery Dungeon", "Hardware", 
+    "Nature", "Lemmings", "Psychedelic", "Sniper", "Tutorial", "4 Player Local" 
+    , "Mars", "Match 3", "Dark Humor", "Audio Production", "Gun Customization",
+    "Western", "Swordplay", "Real-Time", "ATV", "Dinosaurs", "Ninja", "6DOF", "Tactical",
+     "Alternate History", "Transhumanism", "Dog", "Bikes", "Football"
+], axis=1)
+
+X = data.drop(["name"], axis = 1)
+
+print(len(data.columns))
+knn = NearestNeighbors(n_neighbors=15, metric='cosine')
+knn.fit(X)
+
+game_index = 1
+print(X.iloc[game_index])
+distances, indices = knn.kneighbors([X.iloc[game_index]])
+
+print(f"Benzer oyunlar {data['name'].iloc[game_index]} için:")
+for i in indices[0]:
+    print("-> "+data['name'].iloc[i])
+
